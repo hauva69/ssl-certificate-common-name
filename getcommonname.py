bbba5b73552ca6f7d58b1371f2ec0e19d7a4065f
Ari Makela <ari.makela@iki.fi>
2017-08-03 15:11:25 +0300

    Except TypeError, better regexes.

--- getcommonname.py
+++ getcommonname.py
@@ -26,6 +26,8 @@ def is_port_open(host, port):
         return False
     except UnicodeError as ex:
         logging.error('Check the certificate manually for host {0}: {1}'.format(host, ex))
+    except TypeError as ex:
+        logging.error('Check the certificarte manully for host {0}: {1}'.format(host, ex))
     result = sock.connect_ex((ip, port))
     if result == 0:
         return True
@@ -38,10 +40,9 @@ def main():
         logging.debug(host)
         if is_port_open(host, 443):
             print('host={0}'.format(host))
-            cmd = "/bin/echo | /usr/bin/openssl s_client -showcerts -connect {0}:443 2>&1 | /usr/local/bin/awk '/kauppalehti\.fi/ && /CN=/'".format(host)
+            cmd = "/bin/echo | /usr/bin/openssl s_client -showcerts -connect {0}:443 2>&1 | /usr/local/bin/awk '/^subject=.*CN=/'".format(host)
             logging.debug('host={0}'.format(host))
             logging.debug(cmd)
-            print('host={0}'.format(host))
             rc = os.system(cmd)
             if rc != 0:
                 logging.debug('openssl exited with value {0} for hostname {1}'.format(rc, host))
